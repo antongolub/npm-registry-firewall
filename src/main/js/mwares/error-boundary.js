b28537d415697607ef070ef0e81f01626c26fafd
Anton Golub <mailbox@antongolub.ru>
2022-04-06 17:23:38 +0300

    feat: print trace id in error log

--- src/main/js/mwares/error-boundary.js
+++ src/main/js/mwares/error-boundary.js
@@ -4,10 +4,9 @@ export const errorBoundary = async (err, req, res, next) => {
   const message = err.message || err.res?.statusMessage || 'Internal server error'
   const code = err.status || err.res?.statusCode || 500
 
-  logger.error(err)
+  logger.error(`[${res.id}]`, err)
 
   res
     .writeHead(code + '\n')
     .end(message + '\n')
 }
-
