74c29a0b690a3cc8e90c106f293989a265b1de72
Anton Golub <mailbox@antongolub.ru>
2022-04-14 17:00:53 +0300

    fix: fix run on windows

--- src/main/js/semver.js
+++ src/main/js/semver.js
@@ -1,8 +1,10 @@
 import { createRequire } from 'node:module'
-import {spawnSync} from 'node:child_process'
+import { execSync } from 'node:child_process'
+import { join } from 'node:path'
 const require = createRequire(import.meta.url)
 
-const pkgRoot = spawnSync('npm', ['list', '--global', '--parseable', '--depth=1', 'semver'])
-  .stdout.toString().trim().split('\n').sort()[0]
-
-export const semver = require(`${pkgRoot}`)
+export const semver = require(join(
+  execSync('npm list -g --depth=0 --parseable npm', {timeout: 10000, shell: true}).toString().trim(),
+  'node_modules',
+  'semver'
+))
