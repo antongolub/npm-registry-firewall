dcab6f15e72eab17d74ef1e7c3fe2d7f0365e6c9
Anton Golub <mailbox@antongolub.ru>
2022-04-18 01:38:11 +0300

    perf: print error stacktrace

--- src/main/js/mwares/error-boundary.js
+++ src/main/js/mwares/error-boundary.js
@@ -1,11 +1,13 @@
 import {INTERNAL_SERVER_ERROR, statusMessageMap} from '../http/index.js'
 
 export const errorBoundary = async (err, req, res, next) => {
-  const code = err.statusCode || res.statusCode || INTERNAL_SERVER_ERROR
-  const message = err.statusMessage || res.statusMessage || err.message || statusMessageMap[code] || statusMessageMap[INTERNAL_SERVER_ERROR]
+  const code = err.statusCode || INTERNAL_SERVER_ERROR
+  const message = err.statusMessage || err.message || statusMessageMap[code] || statusMessageMap[INTERNAL_SERVER_ERROR]
 
-  req.log.error(err.message || message)
+  req.log.error(err.stack)
 
+  res.statusCode = code
+  res.statusMessage = message
   res.writeHead(code, {
     'Content-Type': 'text/plain',
     'Connection': 'keep-alive',
