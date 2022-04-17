f186feb0542e413fc3eb71e412b7bd0e84e09caf
Anton Golub <mailbox@antongolub.ru>
2022-04-17 20:06:38 +0300

    feat: add audit plugin
    closes #20

--- src/main/js/firewall/plugins/audit.js
+++ src/main/js/firewall/plugins/audit.js
@@ -1,3 +1,51 @@
-export const auditPlugin = ({rule, entry, options}) => {
-  return 'deny'
+import {semver} from '../../semver.js'
+import {request} from '../../http/index.js'
+import {createCache} from '../../cache.js'
+
+const cache = createCache({
+  ttl: 120_000
+})
+
+export const auditPlugin = async ({entry: {name, version}, options = {}, boundContext: {registry}}) => {
+  const severityOrder = [ 'low', 'moderate', 'high', 'critical']
+  const advisories = await getAdvisories(name, registry)
+
+  const vulns = advisories.filter(({vulnerable_versions}) =>
+    semver.satisfies(version, vulnerable_versions))
+
+  const max = severityOrder[Math.max(...vulns.map(({severity}) => severityOrder.indexOf(severity)))]
+  const directive = max && (options[max] || options['any'] || options['*'])
+
+  return directive || false
 }
+
+const getAdvisories = async (name, registry) => {
+  const cached = cache?.get(name)
+  if (cached) {
+    return cached
+  }
+
+  const postData = JSON.stringify({[name]: ['0.0.0']})
+  const headers = {
+    'user-agent': 'npm/8.5.0 node/v16.14.2 darwin x64 workspaces/false',
+    'npm-command': 'audit',
+    'content-type': 'application/json',
+    // 'content-encoding': 'gzip',
+    accept: '*/*',
+    // 'content-length': '45',
+    // 'accept-encoding': 'gzip,deflate',
+  }
+
+  const advisories = JSON.parse((await request({
+    method: 'POST',
+    url: `${registry}/-/npm/v1/security/advisories/bulk`,
+    postData,
+    headers
+  })).body)[name] || []
+
+  cache?.add(name, advisories)
+
+  return advisories
+}
+
+export default auditPlugin
--- src/main/js/mwares/proxy.js
+++ src/main/js/mwares/proxy.js
@@ -1,10 +1,6 @@
 import {request} from '../http/client.js'
 
-export const proxy = (registry) => async (req, res, next) => {
-  if (!registry) {
-    throw new Error('proxy-mware: registry is required')
-  }
-
+export const proxy = (registry) => async (req, res) => {
   const url = `${registry}${req.url}`
   await request({ url, method: req.method, pipe: {req, res}, followRedirects: true})
 }
