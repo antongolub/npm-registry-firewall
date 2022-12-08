324759b42c2189096f302cd45dedfe760c183aba
Anton Golub <antongolub@antongolub.com>
2022-12-08 20:44:59 +0300

    perf: tweak up zip layer

--- src/main/js/zip.js
+++ src/main/js/zip.js
@@ -3,9 +3,13 @@ import os from 'node:os'
 import {promisify} from 'node:util'
 import {Buffer} from 'node:buffer'
 
+import { getConfig } from './config.js'
 import { runWorker } from './worker/index.js'
 
-const cpulen = os.cpus().length
+export const gzip = async (data) => (getConfig()?.workerConcurrency || os.cpus().length) === 1
+  ? promisify(zlib.gzip)(data)
+  : runWorker('worker-zip.js', { method: 'gzip', args: [data] }).then(Buffer.from)
 
-export const gzip = cpulen === 1 ? promisify(zlib.gzip) : async (data) => runWorker('worker-zip.js', { method: 'gzip', args: [data] }).then(Buffer.from)
-export const gunzip = cpulen === 1 ? promisify(zlib.gunzip) : async (data) => runWorker('worker-zip.js', { method: 'gunzip', args: [data] }).then(Buffer.from)
+export const gunzip = async (data) => (getConfig()?.workerConcurrency || os.cpus().length) === 1
+  ? promisify(zlib.gunzip)(data)
+  : runWorker('worker-zip.js', { method: 'gunzip', args: [data] }).then(Buffer.from)
